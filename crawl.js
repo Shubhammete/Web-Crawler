42a7a17d6f066ddbf3a61fba7d32948c8855d28b
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2023-11-05 00:03:06 +0530

    added function to extract links and tests to check if url is valid or not

--- crawl.js
+++ crawl.js
@@ -1,3 +1,34 @@
+const {JSDOM} = require('jsdom')
+
+let getURLFromHTML = (HTMLBody,baseURL) =>{
+    const urls = []
+    let dom = new JSDOM(HTMLBody)  // created JSDOM object and passed HTML code in it
+    let links = dom.window.document.querySelectorAll('a')  // selects all anchor tags in given HTML 
+    for(const link of links){   // iterate through all anchor tags
+        if(link.href.slice(0,1) === '/'){   // checks if link is relative
+            // check if url is valid or not
+            try{                    
+                link.href = `${baseURL}${link.href}`  // add base url to complete it
+                let urlObj = new URL(link.href)  // checks url is valid or not
+                urls.push(urlObj.href)      // push href value to urls array
+            }catch(err){
+                console.log(`Error in relative URL: ${err.message}`)   // return error message
+            }
+            
+        }else{
+            try{
+                let urlObj = new URL(link.href)   // checks url is valid or not
+                urls.push(urlObj.href)          // push href value to urls array
+            }catch(err){
+                console.log(`Error in absolute URL: ${err.message}`)   // return error message
+            }
+        }
+        
+    }
+    
+    return urls
+}
+
 let normalizeURL = (urlString)=>{
     let urlObj = new URL(urlString);  // this already converts string into lower case
     let path = `${urlObj.hostname}${urlObj.pathname}`  // return host name and path name without protocol
@@ -8,5 +39,6 @@ let normalizeURL = (urlString)=>{
 }
 // export module to make function available to other files or modules
 module.exports={
-    normalizeURL
+    normalizeURL,
+    getURLFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,23 +1,61 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL,getURLFromHTML} = require('./crawl.js')
 const {test,expect} = require('@jest/globals')
 //test cases
 test("Checking last / in url",()=>{   // test function has two params testname and function
-    const input = 'https://boot.dev/path/'
+    const input = 'https://test.dev/path/'
     const actual = normalizeURL(input)
-    const expected = 'boot.dev/path'
+    const expected = 'test.dev/path'
     expect(actual).toEqual(expected)  // checks the test
 })
 
 test("Normalize the Captital letters",()=>{
-    const input = 'https://Boot.dev/path/'
+    const input = 'https://TEST.dev/path/'
     const actual = normalizeURL(input)
-    const expected = 'boot.dev/path'
+    const expected = 'test.dev/path'
     expect(actual).toEqual(expected)
 })
 
 test("strips url by removing http",()=>{
-    const input = 'https://Boot.dev/path/'
+    const input = 'https://test.dev/path/'
     const actual = normalizeURL(input)
-    const expected = 'boot.dev/path'
+    const expected = 'test.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test("get absolute url from htmlpage",()=>{
+    const inputHTML = `<html>
+        <body>
+            <a href="https://test.dev/path/"></a>        
+        </body>
+    </html>`
+    const inputBaseURL = "https://test.dev"
+    const actual = getURLFromHTML(inputHTML,inputBaseURL)
+    const expected = ['https://test.dev/path/']
+    expect(actual).toEqual(expected)
+})
+
+test("get relative url from htmlpage",()=>{
+    const inputHTML = `<html>
+        <body>
+            <a href="/path/"></a>        
+        </body>
+    </html>`
+    const inputBaseURL = "https://test.dev"
+    const actual = getURLFromHTML(inputHTML,inputBaseURL)
+    const expected = ['https://test.dev/path/']
+    expect(actual).toEqual(expected)
+})
+
+test("get both url from htmlpage",()=>{
+    const inputHTML = `<html>
+        <body>
+        <a href="https://test.dev/path1/"></a>
+            <a href="/path2/"></a>
+            <a href="invalid"></a>        
+        </body>
+    </html>`
+    const inputBaseURL = "https://test.dev"
+    const actual = getURLFromHTML(inputHTML,inputBaseURL)
+    const expected = ['https://test.dev/path1/','https://test.dev/path2/']
     expect(actual).toEqual(expected)
 })
